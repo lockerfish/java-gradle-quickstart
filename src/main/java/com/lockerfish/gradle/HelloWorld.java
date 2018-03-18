24ec031428d8a0cbaf95406833dee7f4afbcc39c
Hendrix Tavarez <htavarez@gmail.com>
2018-03-18 11:40:10 -0400

    Initial commit

---
+++ src/main/java/com/lockerfish/gradle/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lockerfish.gradle;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/lockerfish/gradle/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.lockerfish.gradle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+	@Test
+	public void HelloLengthIsFive() {
+		assertTrue("Hello".length() == 5);
+
+	}
+
+}
